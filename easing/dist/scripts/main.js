280c3a81fd0864319e2fbdb2ad039633b3d8b0cb
Matt Greer <matt.e.greer@gmail.com>
2013-09-01 09:10:17 -0600

    Drawing all easing functions in easing sandbox

--- easing/dist/scripts/main.js
+++ easing/dist/scripts/main.js
@@ -1,4 +1,6 @@
 (function() {
+  var __hasProp = {}.hasOwnProperty;
+
   require.config({
     paths: {
       plunder: '../plunder/dist/plunder'
@@ -7,10 +9,42 @@
 
   require(['plunder', 'easingVis', 'domReady'], function(Plunder, EasingVis, domReady) {
     'use strict';
+    var createContainer;
+    createContainer = function(key) {
+      var canvas, div, h;
+      div = document.createElement('div');
+      div.style.display = 'inline-block';
+      div.style.width = '300px';
+      div.style.marginLeft = '5px';
+      div.style.marginRight = '5px';
+      h = document.createElement('h3');
+      h.innerHTML = key;
+      div.appendChild(h);
+      canvas = document.createElement('canvas');
+      canvas.width = 300;
+      canvas.height = 200;
+      canvas.id = "canvas-" + key;
+      div.appendChild(canvas);
+      document.body.appendChild(div);
+      return canvas.id;
+    };
     return domReady(function() {
-      new EasingVis('canvas', 'cubicInOut').go();
-      new EasingVis('canvas2', 'bounceInOut').go();
-      return new EasingVis('canvas3', 'quadOut').go();
+      var canvasId, key, value, _ref, _results;
+      canvasId = createContainer('linear');
+      new EasingVis(canvasId, 'linear').go();
+      _ref = Plunder.Easie;
+      _results = [];
+      for (key in _ref) {
+        if (!__hasProp.call(_ref, key)) continue;
+        value = _ref[key];
+        if (typeof value === 'function' && key.indexOf('linear') < 0) {
+          canvasId = createContainer(key);
+          _results.push(new EasingVis(canvasId, key).go());
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
     });
   });
 
--- easing/src/coffee/main.coffee
+++ easing/src/coffee/main.coffee
@@ -5,9 +5,32 @@ require.config
 require ['plunder', 'easingVis', 'domReady'], (Plunder, EasingVis, domReady) ->
   'use strict'
 
+  createContainer = (key) ->
+    div = document.createElement 'div'
+    div.style.display = 'inline-block'
+    div.style.width = '300px'
+    div.style.marginLeft = '5px'
+    div.style.marginRight = '5px'
+    h = document.createElement 'h3'
+    h.innerHTML = key
+    div.appendChild h
+
+    canvas = document.createElement 'canvas'
+    canvas.width = 300
+    canvas.height = 200
+    canvas.id = "canvas-#{key}"
+    div.appendChild canvas
+    document.body.appendChild div
+    return canvas.id
+
+
   domReady ->
-    new EasingVis('canvas', 'cubicInOut').go()
-    new EasingVis('canvas2', 'bounceInOut').go()
-    new EasingVis('canvas3', 'quadOut').go()
+    canvasId = createContainer('linear')
+    new EasingVis(canvasId, 'linear').go()
+
+    for own key,value of Plunder.Easie
+      if typeof value == 'function' and key.indexOf('linear') < 0
+        canvasId = createContainer(key)
+        new EasingVis(canvasId, key).go()
 
 
